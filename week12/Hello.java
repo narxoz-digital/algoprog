008080e6cbc120a7f305914aab105a409f0e6d18
Konstantin Latuta <klatuta@gmail.com>
2023-10-24 13:46:04 +0600

    Add Hello World example

---
+++ week12/Hello.java
@@ -0,0 +1,11 @@
+package week12;
+/*
+Анатомия простой программки на языке Java,
+которая выводит приветствие "Hello World!" на экран
+*/
+
+public class Hello {
+    public static void main(String [] args){
+        System.out.println("Hello World!");
+    }
+}
